1f894cb4015ca2f0a4bb363fb17a55954b03f575
Rene Richard <rene@redsofa.ca>
2023-02-02 16:11:55 -0400

    checkpoint

--- src/python/main.py
+++ src/python/main.py
@@ -1,9 +1,12 @@
 import argparse
 import time
+from river import stream
+
 
 # Modify these to default params suit your context... or just call the python script
 # with your own CLI parameters
-DEFAULT_FILE_NAME = '/Users/richardr/data/bioreactor/AAV-RBD-CD4L.csv'
+DEFAULT_FILE_NAME = '../../datasets/small.csv'
+
 
 def get_args():
     parser = argparse.ArgumentParser(description="Unsupervised, online regression modeling.")
@@ -18,10 +21,21 @@ def main():
 
     print('Program Arguments :')
     args = get_args()
-    print(args)
+
+    input_file = args.input_file_name
+
+    params = {
+            'converters' : {
+                'F1' : float,
+                'F2' : float
+            }
+    }
+    for x, y in stream.iter_csv(input_file, target='TARGET', **params):
+        print(f'New instance : features: {x} -- target: {y}')
+
 
     end_time = time.time()
-    print(f"Total execution time (seconds): str({end_time} - {start_time})")
+    print(f"\nTotal execution time (seconds): str({end_time} - {start_time})")
 
 
 if __name__ == '__main__':
