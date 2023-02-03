3afe0f82aed5a190ab3d8aa2ed771e0c4aaa1a64
Rene Richard <rene@redsofa.ca>
2023-02-02 21:29:22 -0400

    checkpoint

--- src/python/modules/nrc/stream_factory.py
+++ src/python/modules/nrc/stream_factory.py
@@ -7,6 +7,10 @@ class StreamFactory():
     def get_csv_stream(input_file, target, **kwargs):
         return stream.iter_csv(input_file, **kwargs)
 
+    @staticmethod
+    def get_kafka_stream(kafka_topic, bootstrap_servers, *kwargs):
+        raise NotImplementedError('Not implemented yet.')
+
 
 if __name__ == '__main__':
     pass
