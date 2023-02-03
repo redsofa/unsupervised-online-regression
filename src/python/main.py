eb7d6a7af862212529f4d07ab9c5cc4af2ffd9a2
Rene Richard <rene@redsofa.ca>
2023-02-02 21:23:58 -0400

    checkpoint

--- src/python/main.py
+++ src/python/main.py
@@ -1,20 +1,34 @@
 import argparse
 import time
-from river import stream
-from nrc import stream_factory
+import json
+from nrc.stream_factory import StreamFactory
 
 # Modify these to default params suit your context... or just call the python script
 # with your own CLI parameters
 DEFAULT_FILE_NAME = '../../datasets/small.csv'
+DEFAULT_STREAM_PARAMETERS = '../../datasets/small.converter'
 
 
 def get_args():
     parser = argparse.ArgumentParser(description="Unsupervised, online regression modeling.")
     parser.add_argument('-1', '--input_file_name', type=str, required=False, default=DEFAULT_FILE_NAME, help="input file")
-
+    parser.add_argument('-2', '--stream_parameters', type=str, required=False, default=DEFAULT_STREAM_PARAMETERS, help='Stream parameters')
     args = parser.parse_args()
     return args
 
+def load_stream_params(i_params_file):
+    with open(i_params_file) as f:
+        data = f.read()
+    stream_params = json.loads(data)
+    for e in stream_params['converters']:
+        data_type = stream_params['converters'][e]
+        if data_type == 'float':
+            stream_params['converters'][e] = float
+        elif data_type == 'int':
+            stream_params['converters'][e] = int
+        else:
+            stream_params['converters'][e] = str
+    return stream_params
 
 def main():
     start_time = time.time()
@@ -23,14 +37,10 @@ def main():
     args = get_args()
 
     input_file = args.input_file_name
+    stream_params = load_stream_params(args.stream_parameters)
+    data_stream = StreamFactory.get_csv_stream(input_file, **stream_params)
 
-    params = {
-            'converters' : {
-                'F1' : float,
-                'F2' : float
-            }
-    }
-    for x, y in stream.iter_csv(input_file, target='TARGET', **params):
+    for x, y in data_stream:
         print(f'New instance : features: {x} -- target: {y}')
 
 
--- src/python/modules/nrc/stream_factory.py
+++ src/python/modules/nrc/stream_factory.py
@@ -1,20 +1,11 @@
 from river import stream
 
+
 class StreamFactory():
-    def __init__(self):
-        self._params = None
-        self._stream = None
 
-    @property
-    def params(self):
-        return self._params
-
-    @params.setter
-    def params(self, params):
-        self._params = params
-
-    def get_csv_stream(self, input_file, target, **kwargs):
-        return stream.iter_csv(input_file, target, **kwargs)
+    @staticmethod
+    def get_csv_stream(input_file, target, **kwargs):
+        return stream.iter_csv(input_file, **kwargs)
 
 
 if __name__ == '__main__':
