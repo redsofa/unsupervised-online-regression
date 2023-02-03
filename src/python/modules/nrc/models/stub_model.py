b387541b7193ea0ac7a0cb065fbcebeb2a9371d2
Rene Richard <rene@redsofa.ca>
2023-02-03 14:12:58 -0400

    checkpoint

--- src/python/modules/nrc/models/stub_model.py
+++ src/python/modules/nrc/models/stub_model.py
@@ -1,11 +1,12 @@
 from queue import Queue
+from abc import ABC, abstractmethod
 
 
-class StubModel:
+class BaseModel(ABC):
     def __init__(self, i_buffer_size):
         self._name = 'stub_model'
         self._buffer = Queue(i_buffer_size)
-
+    
     @property
     def buffer(self):
         return self._buffer
@@ -14,14 +15,21 @@ class StubModel:
     def name(self):
         return self._name
 
-    def process(self, x, y):
-        print(f'In model : {self._name}')
-        print(f'New instance : features: {x} -- target: {y}')
-
     def clear_buffer(self):
         while not self._buffer.empty(): 
             self._buffer.get()
 
+    @abstractmethod
+    def process(self): 
+        pass
 
+
+
+class StubModel(BaseModel):
+    def process(self, x, y):
+        print(f'In model : {self._name}')
+        print(f'New instance : features: {x} -- target: {y}')
+
+    
 if __name__ == '__main__':
     pass
