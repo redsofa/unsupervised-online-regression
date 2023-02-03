df17f455049ce21cb0a3a07016cf8ad8366dbb0b
Rene Richard <rene@redsofa.ca>
2023-02-03 14:25:05 -0400

    checkpoint

---
+++ src/python/modules/nrc/models/base_model.py
@@ -0,0 +1,27 @@
+from queue import Queue
+from abc import ABC, abstractmethod
+
+
+class BaseModel(ABC):
+    def __init__(self, i_buffer_size, i_name):
+        self._name = i_name
+        self._buffer = Queue(i_buffer_size)
+    
+    @property
+    def buffer(self):
+        return self._buffer
+
+    @property
+    def name(self):
+        return self._name
+
+    def clear_buffer(self):
+        while not self._buffer.empty(): 
+            self._buffer.get()
+
+    @abstractmethod
+    def process(self): 
+        pass
+
+if __name__ == "__main__":
+    pass
--- src/python/modules/nrc/models/stub_model.py
+++ src/python/modules/nrc/models/stub_model.py
@@ -1,28 +1,4 @@
-from queue import Queue
-from abc import ABC, abstractmethod
-
-
-class BaseModel(ABC):
-    def __init__(self, i_buffer_size, i_name):
-        self._name = i_name
-        self._buffer = Queue(i_buffer_size)
-    
-    @property
-    def buffer(self):
-        return self._buffer
-
-    @property
-    def name(self):
-        return self._name
-
-    def clear_buffer(self):
-        while not self._buffer.empty(): 
-            self._buffer.get()
-
-    @abstractmethod
-    def process(self): 
-        pass
-
+from nrc.models.base_model import *
 
 
 class StubModel(BaseModel):
