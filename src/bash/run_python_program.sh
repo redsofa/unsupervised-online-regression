98f5478e6f1e57b6e933095297184485ed15338c
Rene Richard <rene@redsofa.ca>
2023-02-08 12:02:40 -0400

    cp

--- src/bash/run_python_program.sh
+++ src/bash/run_python_program.sh
@@ -5,5 +5,5 @@ python ../python/main.py \
     --input_file_name ../../datasets/small.csv \
     --stream_parameters ../../datasets/small.parameters \
     --buffer_size 10 \
-	--max_samples 15 \
-	--pre_train_size 5
+    --max_samples 15 \
+    --pre_train_size 5
