16e1bba5f627d9244360d0a7f15c264169797662
Rene Richard <rene@redsofa.ca>
2023-02-23 12:30:48 -0400

    checkpoint

--- src/python/modules/nrc/models/runner.py
+++ src/python/modules/nrc/models/runner.py
@@ -23,6 +23,8 @@ class ModelRunner():
         self._settings_valid = False
         self._start_time = None
         self._end_time = None
+        self._stop_run = False
+        self._sample_count = 0
 
     @property
     def start_time(self):
@@ -39,32 +41,49 @@ class ModelRunner():
         else:
             raise Exception("Start or End time has not been set yet.")
 
+    def add_instance(self, x, y):
+        self._sample_count += 1
+        if self._max_samples is not None:
+            if self._sample_count >= self._max_samples:
+                print('Maximum instance count reached. Stopping stream processing.')
+                self._stop_run = True
+                return
+
+
     def run(self):
-        #print(f'Running model : {self._model.name}\n')
-        #self.validate_settings()
+        print('\nLaunching model runner')
+        print('Validating model runner settings')
+        self.validate_settings()
+        print(f'Running model : {self._model.name}')
         self._start_time  = time.time()
 
         for x, y in self._data_stream:
             print(x, y)
             # The add_instance() method figures out if we should stop the run because
             # we have reached the maximum number of records we want to process from the stream.
-            # It also figures out if we are in pre-train or normal stream processing mode.
-            # Additionally, it increments the sample count.
-            #self.add_instance(x, y)
+            self.add_instance(x, y)
 
             # Stop running the algorithm if we this flag has been set.
             # This flag usually means that the maximum number of records to process from the stream
             # has been reached.
-            #if self._stop_run :
-            #    break
+            if self._stop_run :
+                break
 
         self._end_time = time.time()
 
-
     def validate_settings(self):
-         # Check that the stream has been set
+        # Check that the model has been set
+        if self._model is None:
+            raise Exception('Cannot launch the model runner. The model instance has not been set.')
+
+        # Check that the stream has been set
         if self._data_stream is None:
             raise Exception(f'Cannot run the algorithm {self._model.name}, the input data stream has not been set.')
+
+        # Check if TrainTestWindow instance has been set
+        if self._tt_win is None:
+            raise Exceptino(f'Cannot run the algorithm {self._model.name}, the TrainTestWindow instance has not been set.')
+
 
     def set_max_samples(self, max_samples):
         self._max_samples = max_samples
--- src/python/modules/test/test_model_runner.py
+++ src/python/modules/test/test_model_runner.py
@@ -1,10 +1,11 @@
 import unittest
 from nrc.models.runner import ModelRunner
+from nrc.factories.model import ModelFactory
 from nrc.factories.stream import StreamFactory
 from nrc.util.window import TrainTestWindow, RegressionMetricsWindow
 import io
 
-
+# Unit test helper functions
 def get_test_data():
     sample_data = \
             '''c1,c2,y
@@ -22,6 +23,13 @@ def get_test_data():
             2.0,3.1,25
             2.4,3.2,22
             3.0,3.4,25
+            1.0,2.0,23
+            2.2,3.5,22
+            3.0,3.1,21
+            2.0,3.1,20
+            2.4,3.4,22
+            2.4,3.4,22
+            2.4,3.4,22
             2.0,3.2,24
             '''.replace(' ', '')
 
@@ -30,6 +38,12 @@ def get_test_data():
 
 def get_stream_params():
     return {"converters":{"c1":float, "c2":float, 'y':int}, "target":"y"}
+
+def get_test_data_stream():
+    test_data = get_test_data()
+    params = get_stream_params()
+    data_stream = StreamFactory.get_csv_stream(test_data, **params)
+    return data_stream
 
 
 class TestModelRunner(unittest.TestCase):
@@ -41,30 +55,42 @@ class TestModelRunner(unittest.TestCase):
         train_size = round(window_size * 0.8)
         test_size = round(window_size * 0.2)
         buffer_size = round(window_size * 0.4)
+        max_samples = 3
 
         print(f'window_size : {window_size}')
         print(f'train_size : {train_size}')
         print(f'test_size : {test_size}')
         print(f'buffer_size : {buffer_size}')
+        print(f'max_samples : {max_samples}')
 
         # Create the ModelRunner object
         m_run = ModelRunner()
 
         # Configure a CSV stream instance of testing data
-        test_data = get_test_data()
-        params = get_stream_params()
-        data_stream = StreamFactory.get_csv_stream(test_data, **params)
+        data_stream = get_test_data_stream()
 
         # Configure a TrainTest_Window instance
         tt_win = TrainTestWindow(train_size, test_size)
+        
         # Configure regression metrics window instance
         m_met = RegressionMetricsWindow(buffer_size)
+
+        # Get a ScikitLearnRegressionModel instance
+        model = ModelFactory.get_instance(
+                'sklearn_linear_regression_model', 
+                1, 
+                1, 
+                1)
+    
 
         # Configure the ModelRunner instance
         m_run.set_train_test_window(tt_win)
         m_run.set_data_stream(data_stream)
-
+        m_run.set_model(model)
+        m_run.set_max_samples(max_samples)
         m_run.run()
+
+        print(f'Model run time: {m_run.run_time}')
 
         # Initially fail the test to ensure code gets executed in
         # test run
