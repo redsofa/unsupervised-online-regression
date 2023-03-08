eea4ed64c39f2b9fb9c145a43537933384854016
Rene Richard <rene@redsofa.ca>
2023-03-08 15:00:24 -0400

    checkpoint

--- src/bash/experiments/uci_turbine.sh
+++ src/bash/experiments/uci_turbine.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# Command to start the Python program
+
+
+# Command to start the model
+echo 'Launching Model ...'
+
 python ../../python/main.py \
     --input_stream_file ~/data/UCI/pp_gas_emission/gt_2015.csv \
     --stream_parameter_file ~/data/UCI/pp_gas_emission/gt_2015.params \
@@ -9,3 +13,11 @@ python ../../python/main.py \
     --test_samples 400 \
     --buffer_size 2400 \
     --delta_threshold 5
+
+echo '\n'
+
+echo 'Model Evaluation'
+# Command to stat the model evaluation
+python ../../python/evaluate.py \
+    --input_results_file ~/data/UCI/pp_gas_emission/gt_2015_out.csv \
+    --output_plots_file ~/data/UCI/pp_gas_emission/gt_2015_plots.png
--- src/bash/experiments/uci_turbine_eval.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Command to start the Python program
-python ../../python/evaluate.py \
-    --input_results_file ~/data/UCI/pp_gas_emission/gt_2015_out.csv \
-    --output_plots_file ~/data/UCI/pp_gas_emission/gt_2015_plots.png
