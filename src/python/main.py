818d360caebad47e2b44a267adf889b9fb1c1188
Rene Richard <rene@redsofa.ca>
2023-03-12 21:55:57 -0300

    checkpoint

--- src/python/main.py
+++ src/python/main.py
@@ -8,21 +8,91 @@ import pandas as pd
 
 
 def arrs_to_df(x_arr, y_pred_arr, y_true_arr):
-    ret_val = pd.DataFrame({'x': x_arr, 'y_pred': y_pred_arr, 'y_true': y_true_arr}, columns=['x', 'y_pred', 'y_true'])
+    ret_val = pd.DataFrame(
+        {"x": x_arr,
+         "y_pred": y_pred_arr,
+         "y_true": y_true_arr},
+        columns=["x", "y_pred", "y_true"],
+    )
     return ret_val
 
 
 def get_args():
-    parser = argparse.ArgumentParser(description="Unsupervised, online regression modeling.")
-    parser.add_argument('-1', '--input_stream_file', type=str, required=False, default=DEFAULT_INPUT_STREAM_FILE, help="Input file (csv).")
-    parser.add_argument('-2', '--stream_parameter_file', type=str, required=False, default=DEFAULT_STREAM_PARAMETER_FILE, help='Stream configuration file.')
-    parser.add_argument('-3', '--train_samples', type=int, required=False, default=DEFAULT_TRAIN_SAMPLES, help='Number of training samples to use.')
-    parser.add_argument('-4', '--test_samples', type=int, required=False, default=DEFAULT_TEST_SAMPLES, help='Number of test samples to use.')
-    parser.add_argument('-5', '--buffer_size', type=int, required=False, default=DEFAULT_BUFFER_SIZE, help='Data buffer size to use.')
-    parser.add_argument('-6', '--max_samples', type=int, required=False, default=DEFAULT_MAX_SAMPLES, help='Max Number of samples to process.')
-    parser.add_argument('-7', '--delta_threshold', type=float, required=False, default=DEFAULT_DELTA_THRESHOLD, help='Evaluation metrics difference threshold.')
-    parser.add_argument('-8', '--output_csv_file', type=str, required=False, default=DEFAULT_OUTPUT_CSV_FILE, help='Default output CSV file.')
-    parser.add_argument('-9', '--output_stats_file', type=str, required=False, default=DEFAULT_OUTPUT_STATS_FILE, help='Default output CSV file.')
+    parser = argparse.ArgumentParser(
+        description="Unsupervised, online regression modeling."
+    )
+    parser.add_argument(
+        "-1",
+        "--input_stream_file",
+        type=str,
+        required=False,
+        default=DEFAULT_INPUT_STREAM_FILE,
+        help="Input file (csv).",
+    )
+    parser.add_argument(
+        "-2",
+        "--stream_parameter_file",
+        type=str,
+        required=False,
+        default=DEFAULT_STREAM_PARAMETER_FILE,
+        help="Stream configuration file.",
+    )
+    parser.add_argument(
+        "-3",
+        "--train_samples",
+        type=int,
+        required=False,
+        default=DEFAULT_TRAIN_SAMPLES,
+        help="Number of training samples to use.",
+    )
+    parser.add_argument(
+        "-4",
+        "--test_samples",
+        type=int,
+        required=False,
+        default=DEFAULT_TEST_SAMPLES,
+        help="Number of test samples to use.",
+    )
+    parser.add_argument(
+        "-5",
+        "--buffer_size",
+        type=int,
+        required=False,
+        default=DEFAULT_BUFFER_SIZE,
+        help="Data buffer size to use.",
+    )
+    parser.add_argument(
+        "-6",
+        "--max_samples",
+        type=int,
+        required=False,
+        default=DEFAULT_MAX_SAMPLES,
+        help="Max Number of samples to process.",
+    )
+    parser.add_argument(
+        "-7",
+        "--delta_threshold",
+        type=float,
+        required=False,
+        default=DEFAULT_DELTA_THRESHOLD,
+        help="Evaluation metrics difference threshold.",
+    )
+    parser.add_argument(
+        "-8",
+        "--output_csv_file",
+        type=str,
+        required=False,
+        default=DEFAULT_OUTPUT_CSV_FILE,
+        help="Default output CSV file.",
+    )
+    parser.add_argument(
+        "-9",
+        "--output_stats_file",
+        type=str,
+        required=False,
+        default=DEFAULT_OUTPUT_STATS_FILE,
+        help="Default output CSV file.",
+    )
     args = parser.parse_args()
     return args
 
@@ -31,10 +101,11 @@ def drift_handler(**kwargs):
     print(f'Drift detected at prediction number : {kwargs["prediction_count"]}')
     print(f'Value of d is : {kwargs["drift_indicator_value"]}')
 
+
 def main():
     args = get_args()
     # Array of models to run.
-    model_name = 'sklearn_linear_regression_model'
+    model_name = "sklearn_linear_regression_model"
     # stream_params specifies how to transform the input data stream features and which column is
     # the target variable.
     stream_params = load_stream_params(args.stream_parameter_file)
@@ -53,12 +124,11 @@ def main():
 
     # Configure the ModelRunner instance
     m_run = ModelRunner(model_name)
-    m_run.set_train_test_window(tt_win)\
-        .set_data_stream(data_stream)\
-        .set_max_samples(args.max_samples)\
-        .set_buffer(buffer)\
-        .set_threshold(args.delta_threshold)\
-        .set_drift_handler(drift_handler)
+    m_run.set_train_test_window(tt_win).set_data_stream(data_stream).set_max_samples(
+        args.max_samples
+    ).set_buffer(buffer).set_threshold(args.delta_threshold).set_drift_handler(
+        drift_handler
+    )
 
     # Run the model
     for x, y_pred, y_true in m_run.run():
@@ -69,16 +139,16 @@ def main():
     p_df = arrs_to_df(x_arr, y_pred_arr, y_true_arr)
     p_df.to_csv(args.output_csv_file, index=False)
 
-    with open(args.output_stats_file, 'w') as f:
-        f.write('Program Arguments :\n\n')
+    with open(args.output_stats_file, "w") as f:
+        f.write("Program Arguments :\n\n")
         for key, value in vars(args).items():
-            f.write(f'{key} : {value}\n')
-        f.write('\n')
-        f.write(f'Model run time : {m_run.run_time}\n')
-        f.write(f'Prediction count : {len(y_pred_arr)}')
+            f.write(f"{key} : {value}\n")
+        f.write("\n")
+        f.write(f"Model run time : {m_run.run_time}\n")
+        f.write(f"Prediction count : {len(y_pred_arr)}")
 
-    print('Results files saved')
+    print("Results files saved")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
