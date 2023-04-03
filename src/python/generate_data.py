47dae73c4ae442efdb4bb3ce8b2d6b40b3b61aaa
Rene Richard <rene@redsofa.ca>
2023-04-03 13:28:37 -0300

    checkpoint

--- src/python/generate_data.py
+++ src/python/generate_data.py
@@ -1,37 +1,16 @@
-from skmultiflow.data import RegressionGenerator
-from skmultiflow.drift_detection import ADWIN
+from river.datasets import synth
 
-# NOTE THIS WAS GENERATED WITH CHATGPT AND ADAPTED FOR OUR CONTEXT
+# https://github.com/online-ml/river/blob/main/river/datasets/synth/friedman.py
 
+dataset = synth.FriedmanDrift(
+        drift_type='gsg',
+        position=(1, 4),
+        transition_window=2,
+        seed=42
+)
 
-# Define a regression generator with 1 dependent variables and 4 independent variable
-generator = RegressionGenerator(n_features=4, n_targets=1, n_informative=3, random_state=42)
+for x, y in dataset.take(5):
+    print(list(x.values()), y)
 
-# Define a drift detector using ADWIN
-drift_detector = ADWIN()
-
-# Generate 500 samples with no drift
-for i in range(500):
-    X, y = generator.next_sample()
-    # Do something with the data
-
- 
-
-# Introduce a drift at sample 500
-generator.n_targets = 1
-
- 
-
-# Generate 500 samples with drift
-for i in range(500):
-    X, y = generator.next_sample()
-    # Do something with the data
-
- 
-
-    # Check if there's a drift
-    if drift_detector.detected_change():
-        # If there's a drift, reset the drift detector
-        drift_detector.reset()
-        # Introduce a new drift
-        generator.n_targets = 3
+if __name__ == '__main__':
+    pass
