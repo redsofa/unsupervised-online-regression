508e498b572048e5325d80ed3f546de6619f6b05
Rene Richard <rene.richard@nrc-cnrc.gc.ca>
2023-05-15 15:43:25 -0300

    checkpoint

--- src/python/baselines/linear-regression.py
+++ src/python/baselines/linear-regression.py
@@ -5,40 +5,99 @@ from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 from fluire.util.files import mkdir_structure
 import os
+import argparse
 
 
+DEFAULT_RAW_DATA_DIR = '/Users/richardr/data/usup_reg/raw/uci/protein'
+DEFAULT_OUTPUT_DIR = '/Users/richardr/data/usup_reg/work/uci/protein'
+DEFAULT_INPUT_CSV_FILE = 'CASP.csv'
+DEFAULT_OUTPUT_PREDICTIONS_FILE = 'CASP_predictions.csv'
+DEFAULT_OUTPUT_STATS_FILE = 'CASP_stats.txt'
+DEFAULT_TRAIN_SAMPLES = 100
+DEFAULT_TEST_SAMPLES = 20
 
-HOME_DIR = os.path.expanduser('~')
-IN_DIR = '/data/usup_reg/raw/uci/protein'
-IN_FILE = 'CASP.csv'
-
-OUT_DIR = '/data/usup_reg/work/uci/protein'
-PRED_FILE = 'CASP_predictions.csv'
-TRAIN_INSTANCES = 100
-TEST_INSTANCES = 20
-X_COL_INDICES = range(1, 10)
-Y_COL_INDEX = range(0, 1)
-
+DEFAULT_X_COL_INDICES = "1,2,3,4,5,6,7,8,9"
+DEFAULT_Y_COL_INDEX = "0"
 
 
-
-
-'''
-python ../../python/main.py \
-    --raw_data_dir ~/data/usup_reg/raw/synth \
-    --output_dir ~/data/usup_reg/work/synth/$NOW \
-    --input_csv_file synth.csv \
-    --input_csv_param_file synth.params \
-    --output_predictions_file synth_predictions.csv \
-    --output_drifts_csv_file synth_drifts.csv \
-    --output_stats_file synth_stats.txt \
-    --train_samples 100 \
-    --test_samples 20 \
-    --buffer_size 100 \
-    --delta_threshold 0.001
-    #--delta_threshold 3
-'''
-
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="Baseline regression modeling."
+    )
+    parser.add_argument(
+        "-1",
+        "--raw_data_dir",
+        type=str,
+        required=False,
+        default=DEFAULT_RAW_DATA_DIR,
+        help="Root of input files directory for this script.",
+    )
+    parser.add_argument(
+        "-2",
+        "--output_dir",
+        type=str,
+        required=False,
+        default=DEFAULT_OUTPUT_DIR,
+        help="Root of output files directory for this script.",
+    )
+    parser.add_argument(
+        "-3",
+        "--input_csv_file",
+        type=str,
+        required=False,
+        default=DEFAULT_INPUT_CSV_FILE,
+        help="Input CSV file for this script.",
+    )
+    parser.add_argument(
+        "-4",
+        "--output_predictions_file",
+        type=str,
+        required=False,
+        default=DEFAULT_OUTPUT_PREDICTIONS_FILE,
+        help="Predictions output CSV file for this script.",
+    )
+    parser.add_argument(
+        "-5",
+        "--output_stats_file",
+        type=str,
+        required=False,
+        default=DEFAULT_OUTPUT_STATS_FILE,
+        help="File name for the output statistics",
+    )
+    parser.add_argument(
+        "-6",
+        "--train_samples",
+        type=int,
+        required=False,
+        default=DEFAULT_TRAIN_SAMPLES,
+        help="Number of training samples to use.",
+    )
+    parser.add_argument(
+        "-7",
+        "--test_samples",
+        type=int,
+        required=False,
+        default=DEFAULT_TEST_SAMPLES,
+        help="Number of test samples to use.",
+    )
+    parser.add_argument(
+            "-8",
+            "--x_col_indices",
+            type=lambda s: [int(item) for item in s.split(',')],
+            required=False,
+            default=DEFAULT_X_COL_INDICES,
+            help='X column indices',
+    )
+    parser.add_argument(
+            "-9",
+            "--y_col_index",
+            type=lambda s: [int(item) for item in s.split(',')],
+            required=False,
+            default=DEFAULT_Y_COL_INDEX,
+            help='y column index',
+    )
+    args = parser.parse_args()
+    return args
 
 
 def arrs_to_df(x_arr, y_pred_arr, y_true_arr):
@@ -50,32 +109,31 @@ def arrs_to_df(x_arr, y_pred_arr, y_true_arr):
 
 
 def main():
-    src_dir = f'{HOME_DIR}/{IN_DIR}'
-    src_file = f'{src_dir}/{IN_FILE}'
-
-    dst_dir = f'{HOME_DIR}/{OUT_DIR}'
-
-    output_pred_csv_file = f"{dst_dir}/{PRED_FILE}"
-    train_and_test_instances = TRAIN_INSTANCES + TEST_INSTANCES
-
+    args = get_args()
     # Create the output directory if it does not exit.
-    mkdir_structure(dst_dir)
+    mkdir_structure(args.output_dir)
+
+    # Store fully qualified names of files that are used in this script
+    input_csv_file = f"{args.raw_data_dir}/{args.input_csv_file}"
+    output_pred_csv_file = f"{args.output_dir}/{args.output_predictions_file}"
+
+    train_and_test_instances = args.train_samples + args.test_samples
 
     # Load source file into pandas dataframe (pdf for short)
-    pdf = pd.read_csv(src_file)
+    pdf = pd.read_csv(input_csv_file)
 
     total_instances = pdf.shape[0]
     non_train_and_test_instances = total_instances - train_and_test_instances
 
     # Train and test split of the initial instance data
-    train_pdf = pdf.iloc[:TRAIN_INSTANCES]
-    test_pdf = pdf.iloc[TRAIN_INSTANCES:train_and_test_instances]
+    train_pdf = pdf.iloc[:args.train_samples]
+    test_pdf = pdf.iloc[args.train_samples:train_and_test_instances]
 
-    train_X = train_pdf.iloc[:, X_COL_INDICES].values
-    train_y = train_pdf.iloc[:, Y_COL_INDEX].values
+    train_X = train_pdf.iloc[:, args.x_col_indices].values
+    train_y = train_pdf.iloc[:, args.y_col_index].values
 
-    test_X = test_pdf.iloc[:, X_COL_INDICES].values
-    test_y = test_pdf.iloc[:, Y_COL_INDEX].values
+    test_X = test_pdf.iloc[:, args.x_col_indices].values
+    test_y = test_pdf.iloc[:, args.y_col_index].values
 
     # Create linear regression object
     regr = linear_model.LinearRegression()
@@ -86,23 +144,23 @@ def main():
     # Make predictions using the testing set
     pred_y = regr.predict(test_X)
 
+    # Ignore this...
     # The coefficients (FROM TRAINING)
-    print("Coefficients: \n", regr.coef_)
+    # print("Coefficients: \n", regr.coef_)
     # The mean squared error
-    print("Mean squared error: %.2f" % mean_squared_error(test_y, pred_y))
+    # print("Mean squared error: %.2f" % mean_squared_error(test_y, pred_y))
     # The coefficient of determination: 1 is perfect prediction
-    print("Coefficient of determination: %.2f" % r2_score(test_y, pred_y))
-
+    # print("Coefficient of determination: %.2f" % r2_score(test_y, pred_y))
 
     # make predictions on the rest of the data
-    non_train_or_test_pdf = pdf.iloc[train_and_test_instances:non_train_and_test_instances]
-    rest_X = non_train_or_test_pdf.iloc[:, X_COL_INDICES].values
-    rest_y = non_train_or_test_pdf.iloc[:, Y_COL_INDEX].values
+    non_train_or_test_pdf = pdf.iloc[train_and_test_instances:total_instances]
+    rest_X = non_train_or_test_pdf.iloc[:, args.x_col_indices].values
+    rest_y = non_train_or_test_pdf.iloc[:, args.y_col_index].values
 
     pred_rest_y = regr.predict(rest_X)
     print("Mean squared error: %.2f" % mean_squared_error(rest_y, pred_rest_y))
     # The coefficient of determination: 1 is perfect prediction
-    print("Coefficient of determination: %.2f" % r2_score(rest_y, pred_rest_y))
+    print("R2 score : %.2f" % r2_score(rest_y, pred_rest_y))
 
     pred_pdf = arrs_to_df(rest_X.tolist(), pred_rest_y.flatten().tolist(), rest_y.flatten().tolist())
     pred_pdf.to_csv(output_pred_csv_file, index=False)
