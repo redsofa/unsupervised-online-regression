9c5db852a265c55bd3a4f2bd1e2895dda734068c
Rene Richard <rene.richard@nrc-cnrc.gc.ca>
2023-05-25 13:30:24 -0300

    checkpoint

--- src/bash/experiments/run_all.sh
+++ src/bash/experiments/run_all.sh
@@ -10,8 +10,20 @@ export SRC_ROOT="$(pwd)/../../../"
 #sh ./pagehinkley/lr/synth.sh
 #sh ./retrain_every/lr/synth.sh
 
-echo "Running experiments on City of Fredericton air quality data"
-sh ./adwin/lr/fred_aq.sh
-sh ./kswin/lr/fred_aq.sh
-sh ./pagehinkley/lr/fred_aq.sh
-sh ./retrain_every/lr/fred_aq.sh
+#echo "Running experiments on City of Fredericton air quality data"
+#sh ./adwin/lr/fred_aq.sh
+#sh ./kswin/lr/fred_aq.sh
+#sh ./pagehinkley/lr/fred_aq.sh
+#sh ./retrain_every/lr/fred_aq.sh
+
+#echo "Running experiments on UCI wine data"
+#sh ./adwin/lr/uci_wine.sh
+#sh ./kswin/lr/uci_wine.sh
+#sh ./pagehinkley/lr/uci_wine.sh
+#sh ./retrain_every/lr/uci_wine.sh
+
+#echo "Running experiments on UCI protein data"
+sh ./adwin/lr/uci_protein.sh
+#sh ./kswin/lr/uci_protein.sh
+#sh ./pagehinkley/lr/uci_protein.sh
+#sh ./retrain_every/lr/uci_protein.sh
