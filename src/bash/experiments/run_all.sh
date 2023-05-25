f90d0b13cca85d10a219a1906aaaae0392b22b21
Rene Richard <rene.richard@nrc-cnrc.gc.ca>
2023-05-25 19:31:26 -0300

    checkpoint

--- src/bash/experiments/run_all.sh
+++ src/bash/experiments/run_all.sh
@@ -29,7 +29,13 @@ export SRC_ROOT="$(pwd)/../../../"
 #sh ./retrain_every/lr/uci_protein.sh
 
 #echo "Running experiments on UCI std_turbine data"
-sh ./adwin/lr/uci_std_turbine.sh
-sh ./kswin/lr/uci_std_turbine.sh
-sh ./pagehinkley/lr/uci_std_turbine.sh
-sh ./retrain_every/lr/uci_std_turbine.sh
+#sh ./adwin/lr/uci_std_turbine.sh
+#sh ./kswin/lr/uci_std_turbine.sh
+#sh ./pagehinkley/lr/uci_std_turbine.sh
+#sh ./retrain_every/lr/uci_std_turbine.sh
+
+echo "Running experiments on UCI energy data"
+#sh ./adwin/lr/uci_energy.sh
+#sh ./kswin/lr/uci_energy.sh
+#sh ./pagehinkley/lr/uci_energy.sh
+sh ./retrain_every/lr/uci_energy.sh
