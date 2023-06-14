b415211cedad2e955d6f1f5e270d7ad9c973c67c
Rene Richard <rene.richard@nrc-cnrc.gc.ca>
2023-06-14 16:13:09 -0300

    checkpoint

--- src/python/packages/fluire/fluire/models/runner_2.py
+++ src/python/packages/fluire/fluire/models/runner_2.py
@@ -31,9 +31,11 @@ class ModelRunner:
         self._initial_working_datapoints_recieved = False
         self._prediction_count = 0
         self._drift_detector = None
+        self._drift_detector_config = None
         self._drift_handler = None
         self._model_retrained_handler = None
         self._retrain_at_every_sample_count = None
+        self._delta_threshold = 0.01
         logger.debug('ModelRunner construction done. Nothing initalized yet.')
 
     def validate_settings(self) -> None:
@@ -71,23 +73,21 @@ class ModelRunner:
         return self
 
     def set_drift_detector(self, **kwargs) -> ModelRunner:
-        self._drift_detector = kwargs
-        detector = kwargs['detector']
+        self._drift_detector_config = kwargs
+        detector_type = self._drift_detector_config['detector']
         logger.debug(f'detector kwargs : {kwargs}')
-        logger.debug(f'Setting detector : {detector}')
-        if detector == 'NONE':
-            self._drift_detector = None
-        elif detector == 'ADWIN':
-            #self._drift_detector = drift.ADWIN(clock=16, max_buckets=20, min_window_length=50)
+        logger.debug(f'Setting detector_type : {detector_type}')
+        if detector_type == 'NA':
+            self._drift_detector = 'NA'
+        elif detector_type == 'ADWIN':
             self._drift_detector = drift.ADWIN()
-        elif detector == 'KSWIN':
-            self._drift_detector = drift.KSWIN(alpha=0.0001, seed=42)
-        elif detector == 'PAGEHINKLEY':
-            self._drift_detector = drift.PageHinkley()
-        elif detector == 'PERIODIC':
+        elif detector_type == 'PERIODIC':
+            self._drift_detector = 'PERIODIC'
             self._retrain_at_every_sample_count = kwargs['retrain_every']
         else:
-            raise Exception('Invalid drift detector')
+            msg = 'Invalid drift detector type'
+            logger.error(msg)
+            raise Exception(msg)
         return self
 
     def set_scaler(self, obj: Scaler) -> ModelRunner:
@@ -175,7 +175,7 @@ class ModelRunner:
 
     def _add_initial_train_sample(self, sample):
         logger.debug(f'Sample count : {self._sample_count}')
-        logger.info('Adding initial training data.')
+        logger.debug('Adding initial training data.')
         if not self._sliding_window.is_full:
             self._sliding_window.add_data(sample)
             logger.debug(f'Sliding window was not full yet. Current size now : {self._sliding_window.len}.')
@@ -265,26 +265,105 @@ class ModelRunner:
 
     def _is_drift_detected(self):
         logger.debug('Checking for drift.')
-        return True
+        drift_detected = False
 
-    def _calculate_Z2(self):
-        pass
+        if self._drift_detector_config['detector'] == 'ADWIN':
+            drift_detected = self._drift_detector.drift_detected
+
+        if self._drift_detector_config['detector'] == 'PERIODIC':
+            if self._prediction_count % self._retrain_at_every_sample_count == 0:
+                drift_detected = True
+
+        if drift_detected:
+            if self._drift_handler:
+                logger.debug('Calling drift handler.')
+                self._drift_handler(
+                    prediction_count=self._prediction_count,
+                    drift_indicator_value=0
+                )
+        return drift_detected
+
+    def _model_replacement_required(self, Z1, Z2, threshold):
+        logger.debug('Determining if retraining is required.')
+        model_replacement_required = True
+        logger.debug(f'Model replacement required : {model_replacement_required}')
+        return model_replacement_required
+
+    def _update_drift_detector(self, y_pred):
+        if self._drift_detector_config['detector'] == 'ADWIN':
+            self._drift_detector.update(y_pred)
+
+    def _train_new_model(self) -> None:
+        logger.debug('Training new model.')
+        new_model = ModelFactory.get_instance(self._model_name)
+
+        # Transform the training and testing data
+        x_train, y_train = XYTransformers.arr_dict_to_xy(self._sliding_window.get_as_list())
+        x_test, y_test = XYTransformers.arr_dict_to_xy(self._buffer.get_as_list())
+        logger.debug(f'x_train : {x_train}')
+        logger.debug(f'y_train : {y_train}')
+        logger.debug(f'x_test : {x_test}')
+        logger.debug(f'y_test : {y_test}')
+        # Fit the model to the training data
+        new_model.fit(x_train, np.ravel(y_train))
+
+        # Make predictions on the test data
+        y_preds = new_model.predict(x_test)
+
+        # Evaluate the model's performance (compare truth values with predictions) and store
+        # the evaluation metric
+        Z2 = self._model_eval_fn(y_true=y_test, y_pred=y_preds)
+        logger.debug(f'Z2 score : {Z2}')
+        logger.debug('New model trained.')
+
+        return new_model, Z2
 
     def _process_prediction(self, x, y_pred):
         logger.debug('Processing new prediction.')
         xy_ins = XYTransformers.xy_pred_to_numpy_dictionary(x, y_pred)
+        logger.debug('Adding data to buffer.')
         self._buffer.add_data(xy_ins)
+        logger.debug('Updating the sliding window.')
         self._sliding_window.get_and_remove_oldest()
         self._sliding_window.add_data(xy_ins)
+
+        if self._drift_detector != 'NA':
+            logger.debug('Updating the drift detector.')
+            self._update_drift_detector(y_pred)
 
         if self._buffer.is_full:
             logger.debug('Buffer is full.')
             logger.debug(f'Buffer contents : {self._buffer.get_as_list()}')
-            if self._is_drift_detected():
-                logger.debug(f'Drift detected using detector : {self._drift_detector}.')
-                self._calculate_Z2()
-                # if Z1 - Z2 is greater than threshold,
-                #
+            # If we are using a drift detector,
+            if self._drift_detector != 'NA':
+                logger.debug('Drift detector in use.')
+                if self._is_drift_detected():
+                    logger.debug(f'Drift detected using detector : {self._drift_detector}.')
+                    new_model, Z2 = self._train_new_model()
+                    if self._model_replacement_required(self._Z1, Z2, 0.01):
+                        logger.debug('Model replacement required.')
+                        logger.debug('Replacing existing model with new model.')
+                        self._model = new_model
+                        self._Z1 = Z2
+                        if self._model_retrained_handler:
+                            logger.debug('Calling model retrain handler.')
+                            self._model_retrained_handler(model=self._model)
+                    else:
+                        logger.debug('No model replacement required.')
+                else:
+                    logger.debug(f'No drift detected using detector : {self._drift_detector}.')
+            # If no drift detector is being used,
+            else:
+                logger.debug('Drift detector not in use.')
+                new_model, Z2 = self._train_new_model()
+                if self._model_replacement_required(self._Z1, Z2, self._delta_threshold):
+                    logger.debug('Model replacement required.')
+                    logger.debug('Replacing existing model with new model.')
+                    self._model = new_model
+                    self._Z1 = Z2
+                else:
+                    logger.debug('No model replacement required.')
+
             logger.debug('Clearing buffer contents.')
             self._buffer.clear_contents()
 
