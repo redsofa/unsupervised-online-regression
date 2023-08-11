67f75c960290524caf243fd86a10e4bf4abbb731
Rene Richard <rene.richard@nrc-cnrc.gc.ca>
2023-08-11 11:42:26 -0300

    checkpoint

--- src/bash/experiments/baseline/lr/air_quality_COGT.sh
+++ src/bash/experiments/baseline/lr/air_quality_COGT.sh
@@ -2,14 +2,15 @@
 # exit when any command fails
 set -e
 
-NOW=$( date "+%Y_%m_%d__%H_%M_%S" )
-cd ${SRC_ROOT}/src/python
+cd ${PROJ_ROOT}/src/python
+
+echo "Running baseline experiment ..."
 
 echo 'Launching Adaptive Model ...'
 # Command to start the model
 python main.py \
-    --raw_data_dir ~/data/usup_reg/raw/uci/air_quality \
-    --output_dir ~/data/usup_reg/work/baseline/lr/air_quality/$NOW \
+    --raw_data_dir ${DATA_ROOT} \
+    --output_dir ${WORK_ROOT}/baseline/lr/air_quality_COGT/${NOW} \
     --input_csv_file std_AirQualityUCI.csv \
     --input_csv_param_file std_AirQualityUCI_COGT.params \
     --output_predictions_file std_AirQualityUCI_COGT_predictions.csv \
@@ -27,10 +28,16 @@ echo '\n'
 
 echo 'Adaptive Model Evaluation'
 # Command to stat the model evaluation
-python evaluate.py \
-    --output_dir ~/data/usup_reg/work/baseline/lr/air_quality/$NOW \
+python ./evaluation/evaluate.py \
+    --output_dir ${WORK_ROOT}/baseline/lr/air_quality_COGT/${NOW} \
     --predictions_file std_AirQualityUCI_COGT_predictions.csv \
-    --drift_file std_AirQualityUCI_COGT_drifts.csv \
-    --stats_file std_AirQualityUCI_COGT_stats.txt \
-    --plot_file std_AirQualityUCI_COGT.png \
-    --plot_drifts False
+    --stats_file std_AirQualityUCI_COGT_stats.txt
+
+echo '\n'
+
+echo  'Plotting results ...'
+python ./plotting/main.py \
+    --input_dir ${WORK_ROOT}/baseline/lr/air_quality_COGT/${NOW} \
+    --output_dir ${WORK_ROOT}/baseline/lr/air_quality_COGT/${NOW} \
+    --predictions_file std_AirQualityUCI_COGT_predictions.csv \
+    --drift_file std_AirQualityUCI_COGT_drifts.csv
--- src/bash/experiments/run_air_quality.sh
+++ src/bash/experiments/run_air_quality.sh
@@ -2,24 +2,45 @@
 # exit when any command fails
 set -e
 
-export SRC_ROOT="$(pwd)/../../../"
+export NOW=$( date "+%Y_%m_%d__%H_%M_%S" )
+export PROJ_ROOT="$(pwd)/../../../"
+export DATA_ROOT=~/data/usup_reg/raw/uci/air_quality
+export WORK_ROOT=~/data/usup_reg/work
+
 
 echo "Running experiments on air_quality data COGT prediction"
-sh ./baseline/lr/air_quality_COGT.sh > air_quality_COGT_baseline.log
-sh ./adwin_with_z1_z2/lr/air_quality_COGT.sh > air_quality_COGT_adwin_with_z1_z2.log
-sh ./z1_z2_only/lr/air_quality_COGT.sh > air_quality_COGT_z1_z2_only.log
 
-echo "Running experiments on air_quality data NMHCGT prediction"
-sh ./baseline/lr/air_quality_NMHCGT.sh > air_quality_NMHCGT_baseline.log
-sh ./adwin_with_z1_z2/lr/air_quality_NMHCGT.sh > air_quality_NMHCGT_adwin_with_z1_z2.log
-sh ./z1_z2_only/lr/air_quality_NMHCGT.sh > air_quality_NMHCGT_z1_z2_only.log
+mkdir -p ${WORK_ROOT}/baseline/lr/air_quality_COGT/${NOW}
+sh ./baseline/lr/air_quality_COGT.sh &> ${WORK_ROOT}/baseline/lr/air_quality_COGT/${NOW}/air_quality_COGT_baseline.log
 
-echo "Running experiments on air_quality data NOXGT prediction"
-sh ./baseline/lr/air_quality_NOXGT.sh > air_quality_NOXGT_baseline.log
-sh ./adwin_with_z1_z2/lr/air_quality_NOXGT.sh > air_quality_NOXGT_adwin_with_z1_z2.log
-sh ./z1_z2_only/lr/air_quality_NOXGT.sh > air_quality_NOXGT_z1_z2_only.log
+# mkdir -p ${WORK_ROOT}/adwin_with_z1_z2lr/air_quality_COGT/${NOW}
+# sh ./adwin_with_z1_z2/lr/air_quality_COGT.sh &> ${WORK_ROOT}/adwin_with_z1_z2lr/air_quality_COGT/${NOW}/air_quality_COGT_adwin_with_z1_z2.log
 
-echo "Running experiments on air_quality data NO2 prediction"
-sh ./baseline/lr/air_quality_NO2.sh > air_quality_NO2_baseline.log
-sh ./adwin_with_z1_z2/lr/air_quality_NO2.sh > air_quality_NO2_adwin_with_z1_z2.log
-sh ./z1_z2_only/lr/air_quality_NO2.sh > air_quality_NO2_z1_z2_only.log
+# mkdir -p ${WORK_ROOT}/z1_z2_only/air_quality_COGT/${NOW}
+# sh ./z1_z2_only/lr/air_quality_COGT.sh &> ${WORK_ROOT}/z1_z2_only/air_quality_COGT/${NOW}/air_quality_COGT_z1_z2_only.log
+
+
+
+# echo "Running experiments on air_quality data NO2 prediction"
+# 
+# mkdir -p ${WORK_ROOT}/baseline/lr/air_quality_NO2/${NOW}
+# sh ./baseline/lr/air_quality_NO2.sh &> ${WORK_ROOT}/baseline/lr/air_quality_NO2/${NOW}/air_quality_NO2_baseline.log
+
+# mkdir -p ${WORK_ROOT}/adwin_with_z1_z2/lr/air_quality_NO2/${NOW}
+# sh ./adwin_with_z1_z2/lr/air_quality_NO2.sh &> ${WORK_ROOT}/adwin_with_z1_z2/lr/air_quality_NO2/${NOW}/air_quality_NO2_adwin_with_z1_z2.log
+# 
+# mkdir -p ${WORK_ROOT}/z1_z2_only/lr/air_quality_NO2/${NOW}
+# sh ./z1_z2_only/lr/air_quality_NO2.sh &> ${WORK_ROOT}/z1_z2_only/lr/air_quality_NO2/${NOW}/air_quality_NO2_z1_z2_only.log
+# 
+# 
+# 
+# echo "Running experiments on air_quality data NMHCGT prediction"
+# 
+# mkdir -p ${WORK_ROOT}/baseline/lr/air_quality_NMHCGT/${NOW}
+# sh ./baseline/lr/air_quality_NMHCGT.sh &> ${WORK_ROOT}/baseline/lr/air_quality_NMHCGT/${NOW}/air_quality_NMHCGT_baseline.log
+# 
+# mkdir -p ${WORK_ROOT}/adwin_with_z1_z2/lr/air_quality_NMHCGT/${NOW}
+# sh ./adwin_with_z1_z2/lr/air_quality_NMHCGT.sh &> ${WORK_ROOT}/adwin_with_z1_z2/lr/air_quality_NMHCGT/${NOW}/air_quality_NMHCGT_adwin_with_z1_z2.log
+# 
+# mkdir -p ${WORK_ROOT}/z1_z2_only/lr/air_quality_NMHCGT/${NOW}
+# sh ./z1_z2_only/lr/air_quality_NMHCGT.sh &> ${WORK_ROOT}/z1_z2_only/lr/air_quality_NMHCGT/${NOW}/air_quality_NMHCGT_z1_z2_only.log
